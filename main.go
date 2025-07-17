f654da425ee1b3558ad5999f4e69be968bdd1eff
pheethy <Bizcuitware@gmail.com>
2025-07-17 10:12:31 +0700

    Init DeleteFileFrom MiniO

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"demo-minio/config"
 
@@ -20,13 +19,32 @@ func main() {
 		logrus.Println("connent minio failed:", err.Error())
 	}
 
+	/* Upload to MiniO */
 	ctx := context.Background()
-	fileContent := []byte("Hello MiniO")
+	// fileContent := []byte("Hello MiniO")
 	fileName := "photo/test.txt"
-	uploadInfo, err := minioClient.PutObject(ctx, cfg.MiniO().GetBucket(), fileName, bytes.NewReader(fileContent), int64(len(fileContent)), minio.PutObjectOptions{})
-	if err != nil {
-		logrus.Println("upload file failed:", err.Error())
-	}
+	// uploadInfo, err := minioClient.PutObject(ctx, cfg.MiniO().GetBucket(), fileName, bytes.NewReader(fileContent), int64(len(fileContent)), minio.PutObjectOptions{})
+	// if err != nil {
+	// 	logrus.Println("upload file failed:", err.Error())
+	// }
 
-	logrus.Info("upload file success:", uploadInfo)
+	// logrus.Info("upload file success:", uploadInfo)
+
+	/* Download from MiniO */
+	// obj, err := minioClient.GetObject(ctx, cfg.MiniO().GetBucket(), fileName, minio.GetObjectOptions{})
+	// if err != nil {
+	// 	logrus.Println("download file failed:", err.Error())
+	// }
+
+	// info, err := obj.Stat()
+	// if err != nil {
+	// 	logrus.Println("stat file failed:", err.Error())
+	// }
+
+	// logrus.Info("download file success:", info)
+
+	/* Delete from MiniO */
+	if err := minioClient.RemoveObject(ctx, cfg.MiniO().GetBucket(), fileName, minio.RemoveObjectOptions{}); err != nil {
+		logrus.Println("delete file failed:", err.Error())
+	}
 }
